8f4f68250f1b465967c0be3472c61944620579ea
aashumali7 <aashishmali223@gmail.com>
2023-06-15 19:30:58 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+// there are 2 thing related with functions
+
+//2. fuction calling/invoke 
+
+getmyname();
+getMySurname();
+
+//1. function defination is a one time process
+//bad software enginner
+function getmyname(){ // this is not a per the CamleCase standard
+    console.log("aashu");
+
+}
+
+function getMySurname(){ // this function name is a per the CamleCase standard  
+
+    // Function body
+    // inside the function body i can write many statements
+    console.log("MALI"); //statement = instructions 
+}
+
+ //2. function call
+ getMyAddress("Neemuch MP INDIA");
+
+ //1. function the defination 
+function getMyAddress(addr){
+console.log(addr)
+}
+
---
+++ js/script2.js
@@ -0,0 +1,15 @@
+// lets a  create a function which can add 2 numbers
+
+
+//1. Function defination 
+
+function addTwoNumbers(n1,n2){
+    // every function return something 
+    return n1 + n2
+
+}
+//2. function calling (many time)
+    // associativity principle
+// left = Right 
+ var result = addTwoNumbers(10,20);
+ console.log(result);
